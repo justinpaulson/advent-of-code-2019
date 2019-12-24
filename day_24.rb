d540c30f699d83df1a7e904d21330245349bdc16
Justin Paulson <justinapaulson@gmail.com>
2019-12-24 01:11:17 -0600

    Day 24 refactor

--- day_24.rb
+++ day_24.rb
@@ -1,10 +1,10 @@
 inputs = File.readlines(ARGV[0])
 
-def print_grid(grid, with_z = false)
+def print_grid(grid, z = false)
   0.upto(4) do |y|
     line = ''
     0.upto(4) do |x|
-      if with_z
+      if z
         line += grid[[x,y,0]] || ' '
       else
         line += grid[[x,y]] || ' '
@@ -14,12 +14,9 @@ def print_grid(grid, with_z = false)
   end
 end
 
-chart = {}
-
-inputs.each_with_index do |line, y|
-  line.split('').each_with_index do |input, x|
-    chart[[x,y]] = input
-  end
+def adjacent_bugs_flat(chart, location)
+  x, y = location
+  [[x+1,y],[x-1,y],[x,y+1],[x,y-1]].inject(0){|i, cell| i + (chart[cell] == '#' ? 1 : 0)}
 end
 
 def same_layout(l1, l2)
@@ -31,38 +28,22 @@ def same_layout(l1, l2)
   true
 end
 
+chart = {}
+inputs.each_with_index do |line, y|
+  line.split('').each_with_index do |input, x|
+    chart[[x,y]] = input
+  end
+end
+
 new_grid = {}
-
 layouts = []
-
 while !layouts.any?{|l| same_layout(l, new_grid)}
   layouts << new_grid.clone
   0.upto(4) do |x|
     0.upto(4) do |y|
-      if chart[[x,y+1]] == '#' && chart[[x-1,y]] != '#' && chart[[x+1,y]] != '#' && chart[[x,y-1]] != '#'
+      bugs = adjacent_bugs_flat(chart, [x,y])
+      if bugs == 1 || (bugs == 2 && chart[[x,y]] == '.')
         new_grid[[x,y]] = '#'
-      elsif chart[[x,y+1]] != '#' && chart[[x-1,y]] == '#' && chart[[x+1,y]] != '#' && chart[[x,y-1]] != '#'
-        new_grid[[x,y]] = '#'
-      elsif chart[[x,y+1]] != '#' && chart[[x-1,y]] != '#' && chart[[x+1,y]] == '#' && chart[[x,y-1]] != '#'
-        new_grid[[x,y]] = '#'
-      elsif chart[[x,y+1]] != '#' && chart[[x-1,y]] != '#' && chart[[x+1,y]] != '#' && chart[[x,y-1]] == '#'
-        new_grid[[x,y]] = '#'
-      elsif chart[[x,y]] == '.'
-        if chart[[x,y+1]] == '#' && chart[[x-1,y]] == '#' && chart[[x+1,y]] != '#' && chart[[x,y-1]] != '#'
-          new_grid[[x,y]] = '#'
-        elsif chart[[x,y+1]] == '#' && chart[[x-1,y]] != '#' && chart[[x+1,y]] == '#' && chart[[x,y-1]] != '#'
-          new_grid[[x,y]] = '#'
-        elsif chart[[x,y+1]] == '#' && chart[[x-1,y]] != '#' && chart[[x+1,y]] != '#' && chart[[x,y-1]] == '#'
-          new_grid[[x,y]] = '#'
-        elsif chart[[x,y+1]] != '#' && chart[[x-1,y]] == '#' && chart[[x+1,y]] == '#' && chart[[x,y-1]] != '#'
-          new_grid[[x,y,]] = '#'
-        elsif chart[[x,y+1]] != '#' && chart[[x-1,y]] == '#' && chart[[x+1,y]] != '#' && chart[[x,y-1]] == '#'
-          new_grid[[x,y,]] = '#'
-        elsif chart[[x,y+1]] != '#' && chart[[x-1,y]] != '#' && chart[[x+1,y]] == '#' && chart[[x,y-1]] == '#'
-          new_grid[[x,y,]] = '#'
-        else
-          new_grid[[x,y]] = '.'
-        end
       else
         new_grid[[x,y]] = '.'
       end
@@ -71,25 +52,8 @@ while !layouts.any?{|l| same_layout(l, new_grid)}
   chart = new_grid.clone
 end
 
-total = 0
-0.upto(25) do |power|
-  if new_grid[[power % 5, power / 5]] == '#'
-    total += 2 ** power
-  end
-end
-
 print_grid(new_grid)
-
-puts "Biodiversity Rating: #{total}"
-
-
--100.upto(100) do |z|
-  0.upto(4) do |x|
-   0.upto(4) do |y|
-    chart[[x,y,z]] = '.'
-   end
-  end
-end
+puts "Biodiversity Rating: #{0.upto(24).inject(0){|i, power| new_grid[[power % 5, power / 5]] == '#' ? i + 2 ** power : i}}"
 
 inputs.each_with_index do |line, y|
   line.split('').each_with_index do |input, x|
@@ -101,9 +65,7 @@ chart[[2,2,0]] = '?'
 
 def adjacent_bugs(chart, location)
   bugs = 0
-  x = location[0]
-  y = location[1]
-  z = location[2]
+  x, y, z = location
   if [[1,1],[3,1],[1,3],[3,3]].include?([x,y])
     if chart[[x+1,y,z]] == '#'
       bugs += 1
@@ -270,7 +232,7 @@ end
 
 new_grid = {}
 new_grid[[2,2,0]] = '?'
-1.upto(200) do |s|
+1.upto(200) do |_|
   0.upto(4) do |x|
     0.upto(4) do |y|
       -100.upto(100) do |z|
@@ -298,4 +260,3 @@ end
 
 print_grid(new_grid, true)
 puts "Total Bugs: #{total}"
-
